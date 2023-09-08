70bc01d44ed4fc79ea8dbf0977f887cff6c109ad
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2023-09-08 22:17:27 +0700

    fixed course register

--- course-app/src/Pages/Home.tsx
+++ course-app/src/Pages/Home.tsx
@@ -136,15 +136,11 @@ const Home = () => {
             />
             <div className="col-span-2 text-center">
               <p className="overflow-hidden font-thin text-xs text-gray-400">
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
-                aasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkkaasdfmakjjjjjjjjjjjjjjjjjasdjcadadlkcadmakjjjjjjjjjjjjjjjjjjjjkk
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum.
               </p>
               <div className="mt-5">
                 <button
